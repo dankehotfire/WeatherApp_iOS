489d1f2e00c729374b16462df084f9d2e10c0a53
Danil Nurgaliev <Danilkchik@gmail.com>
2021-05-20 12:05:14 +0300

    The repository were initialised

---
+++ MyWeather/MyWeather/View Controllers/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by Danil Nurgaliev on 20.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
